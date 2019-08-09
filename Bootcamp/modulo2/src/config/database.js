0f131347f0fe543a5058fae4914fe4ec48c9bdb6
Vitor <vitorvcs@hotmail.com>
2019-08-09 15:10:28 -0300

    Module 2

---
+++ Bootcamp/modulo2/src/config/database.js
@@ -0,0 +1,13 @@
+module.exports = {
+  dialect: 'postgres',
+  host: '127.0.0.1',
+  username: 'root',
+  password: '',
+  database: 'gonodemodulo2',
+  operatorAliases: false,
+  define: {
+    timestamps: true,
+    underscorded: true,
+    underscoredAll: true
+  }
+}
