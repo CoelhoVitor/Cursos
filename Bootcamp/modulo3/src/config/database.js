5e5c87784609709d3815f1152aa9f7087f292c8d
Vitor <vitorvcs@hotmail.com>
2019-08-20 15:48:38 -0300

    Token

--- Bootcamp/modulo3/src/config/database.js
+++ Bootcamp/modulo3/src/config/database.js
@@ -1,3 +1,3 @@
 module.exports = {
-  uri: 'mongodb+srv://root:<password>@mongonode-ozyb8.mongodb.net/test?retryWrites=true&w=majority'
+  uri: 'mongodb+srv://root:root@mongonode-ozyb8.mongodb.net/test?retryWrites=true&w=majority'
 }
