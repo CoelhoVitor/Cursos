273ce2cfe6338fc030870147625f7d84f5146fd7
Vitor <vitorvcs@hotmail.com>
2019-10-16 11:07:46 -0300

    Fase 1

--- Bootcamp/modulo4/src/components/TechList.js
+++ Bootcamp/modulo4/src/components/TechList.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 
+import TechItem from './TechItem'
+
 class TechList extends Component {
   /* States do React são imutáveis
    * para realizar mudanças é preciso usar o setState()
@@ -7,12 +9,27 @@ class TechList extends Component {
    */
   state = {
     newTech: '',
-    techs: [
-      'Node.js',
-      'ReactJS',
-      'React Native'
-    ]
+    techs: []
   }
+
+  // Executado assim que o componente aparece em tela
+  componentDidMount() {
+    const techs = localStorage.getItem('techs')
+
+    if (techs) {
+      this.setState({ techs: JSON.parse(techs)})
+    }
+  }
+
+  // Executado sempre que houver alterações nas props ou estado
+  componentDidUpdate(_, prevState) {
+    if (prevState.techs !== this.state.techs) {
+      localStorage.setItem('techs', JSON.stringify(this.state.techs))
+    }
+  }
+
+  // Executado quando o componente deixa de existir
+  componentWillUnmount() {}
 
   handleInputChange = e => {
     this.setState({ newTech: e.target.value })
@@ -27,13 +44,24 @@ class TechList extends Component {
     })
   }
 
+  handleDelete = (tech) => {
+    // retorna pro array todas as techs que são diferentes da tech parâmetro
+    this.setState({ techs: this.state.techs.filter(t => t !== tech) })
+  }
+
   render() {
     return (
       <form onSubmit={this.handleSubmit}>
         <h1>{this.state.newTech}</h1>
         <ul>
           {/* {} para adicionar códigos javascript */}
-          {this.state.techs.map(tech => <li key={tech}>{tech}</li>)}
+          {this.state.techs.map(tech => (
+            <TechItem
+            key={tech}
+            tech={tech}
+            onDelete={() => this.handleDelete(tech)}
+            />
+          ))}
         </ul>
         <input
           type="text"
