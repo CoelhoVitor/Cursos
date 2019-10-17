7fbbc63d4fe8d69f0cf0575acba68e54da81ed86
Vitor <vitorvcs@hotmail.com>
2019-10-17 13:15:48 -0300

    Routes

---
+++ Bootcamp/modulo5/src/routes.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+import Main from './pages/Main';
+import Repository from './pages/Repository';
+
+export default function Routes() {
+  return (
+    // fica em volta das rotas
+    <BrowserRouter>
+      {/* garante que apenas uma rota seja chamada por momento */}
+      <Switch>
+        <Route path="/" exact component={Main} />
+        <Route path="/repository" component={Repository} />
+      </Switch>
+    </BrowserRouter>
+  );
+}
