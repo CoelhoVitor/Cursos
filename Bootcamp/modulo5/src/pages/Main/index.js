6ed94779d85acd70024ee86448e2319b6824ba97
Vitor <vitorvcs@hotmail.com>
2019-10-18 14:29:33 -0300

    Estilizando Main

--- Bootcamp/modulo5/src/pages/Main/index.js
+++ Bootcamp/modulo5/src/pages/Main/index.js
@@ -1,7 +1,28 @@
 import React from 'react';
 
-import { Title } from './styles';
+import { FaGithubAlt, FaPlus } from 'react-icons/fa';
+
+import { Container, Form, SubmitButton } from './styles';
 
 export default function Main() {
-  return <Title>Hello World</Title>;
+  return (
+    <Container>
+      <h1>
+        <FaGithubAlt />
+        Repositórios
+      </h1>
+
+      <Form onubmit={() => {}}>
+        <input type="text" placeholder="Adicionar um repositório" />
+
+        {/* Sempre que precisar estilizar um elemento baseado
+        em uma propriedade que ele recebe,
+        esse componente deve virar um novo componente
+        dentro do StyledComponents  */}
+        <SubmitButton disabled>
+          <FaPlus color="#FFF" size={14} />
+        </SubmitButton>
+      </Form>
+    </Container>
+  );
 }
