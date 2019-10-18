439769020aa525c5f68f930110ea766ee4f607a4
Vitor <vitorvcs@hotmail.com>
2019-10-18 16:48:54 -0300

    Adicionando repositorios

--- Bootcamp/modulo5/src/pages/Main/index.js
+++ Bootcamp/modulo5/src/pages/Main/index.js
@@ -1,28 +1,75 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { FaGithubAlt, FaPlus, FaSpinner } from 'react-icons/fa';
 
-import { FaGithubAlt, FaPlus } from 'react-icons/fa';
+import api from '../../services/api';
 
 import { Container, Form, SubmitButton } from './styles';
 
-export default function Main() {
-  return (
-    <Container>
-      <h1>
-        <FaGithubAlt />
-        Repositórios
-      </h1>
+export default class Main extends Component {
+  // CoelhoVitor/Desafios_Bootcamp
+  // rocketseat/unform
+  state = {
+    newRepo: '',
+    repositories: [],
+    loading: false,
+  };
 
-      <Form onubmit={() => {}}>
-        <input type="text" placeholder="Adicionar um repositório" />
+  handleInputChange = e => {
+    this.setState({ newRepo: e.target.value });
+  };
 
-        {/* Sempre que precisar estilizar um elemento baseado
-        em uma propriedade que ele recebe,
-        esse componente deve virar um novo componente
-        dentro do StyledComponents  */}
-        <SubmitButton disabled>
-          <FaPlus color="#FFF" size={14} />
-        </SubmitButton>
-      </Form>
-    </Container>
-  );
+  handleSubmit = async e => {
+    e.preventDefault();
+
+    this.setState({ loading: true });
+
+    const { newRepo, repositories } = this.state;
+
+    const response = await api.get(`/repos/${newRepo}`);
+
+    const data = {
+      name: response.data.full_name,
+    };
+
+    this.setState({
+      repositories: [...repositories, data],
+      newRepo: '',
+      loading: false,
+    });
+  };
+
+  render () {
+    const { newRepo, loading } = this.state;
+
+    return (
+      <Container>
+        <h1>
+          <FaGithubAlt />
+          Repositórios
+        </h1>
+
+        <Form onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            placeholder="Adicionar um repositório"
+            value={newRepo}
+            onChange={this.handleInputChange}
+          />
+
+          {/* Sempre que precisar estilizar um elemento baseado
+          em uma propriedade que ele recebe,
+          esse componente deve virar um novo componente
+          dentro do StyledComponents  */}
+          <SubmitButton loading={loading}>
+            {loading ? (
+              <FaSpinner color="#FFF" size={14} />
+            ) : (
+              <FaPlus color="#FFF" size={14} />
+            )}
+
+          </SubmitButton>
+        </Form>
+      </Container>
+    );
+  }
 }
