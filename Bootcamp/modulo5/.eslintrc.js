01cd3727181fcf39facf32471db8973813404c31
Vitor <vitorvcs@hotmail.com>
2019-11-01 14:33:25 -0300

    Primeiro projeto ReactJS

--- Bootcamp/modulo5/.eslintrc.js
+++ Bootcamp/modulo5/.eslintrc.js
@@ -31,6 +31,7 @@ module.exports = {
       { extensions: ['.jsx', '.js'] }
     ],
     'import/prefer-default-export': 'off',
-    'react/state-in-constructor': ['off', 'never']
+    'react/state-in-constructor': ['off', 'never'],
+    'react/static-property-placement': 'off'
   },
 };
