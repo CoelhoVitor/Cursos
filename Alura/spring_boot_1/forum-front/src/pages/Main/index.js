792c7546967e07990603688834ac937b9c5ed2ff
Vitor <vitorvcs@hotmail.com>
2019-11-24 01:31:22 -0300

    forum-front test

---
+++ Alura/spring_boot_1/forum-front/src/pages/Main/index.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+
+import api from '../../services/api';
+
+import { Text } from './styles';
+
+export default class Main extends Component {
+  state = {
+    topicos: [],
+  };
+
+  // async componentDidMount() {
+  //   const topicos = await api.get("/topicos/:1");
+  //   this.setState({ topicos: JSON.parse(topicos) });
+  // }
+
+  async componentDidMount() {
+    api.get('/').then(response => {console.log(response.data);});
+  }
+  
+  render() {
+    const { topicos } = this.state;
+
+    return (
+      <Text>
+        <h1>Hello World 2!</h1>
+        {topicos.map(topico => (
+          <li key={topico.titulo}>
+            <span>{topico.titulo}</span>
+          </li>
+        ))}
+      </Text>
+    )
+  }
+}
---
+++ Alura/spring_boot_1/forum-front/src/pages/Main/styles.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const Text = styled.form`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  color: #ff6b6b;
+`;
