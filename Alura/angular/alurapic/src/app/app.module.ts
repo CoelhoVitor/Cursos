181aa9cae522d212445ea2b2633e62cb990e7c21
CoelhoVitor <vitorvcs@hotmail.com>
2020-01-07 22:06:20 -0300

    module3 angular course

--- Alura/angular/alurapic/src/app/app.module.ts
+++ Alura/angular/alurapic/src/app/app.module.ts
@@ -1,14 +1,16 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
+import { HttpClientModule } from "@angular/common/http";
 
 import { AppRoutingModule } from "./app-routing.module";
+
+import { PhotoModule } from "./photos/photos.module";
+
 import { AppComponent } from "./app.component";
 
-import { PhotoComponent } from "./photo/photo.component";
-
 @NgModule({
-  declarations: [AppComponent, PhotoComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, PhotoModule],
   providers: [],
   bootstrap: [AppComponent]
 })
