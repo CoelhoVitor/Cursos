a9895355bf17516446413bdaf54e9d39a5ffcf4b
CoelhoVitor <vitorvcs@hotmail.com>
2020-01-09 22:38:46 -0300

    module6 angular course

--- Alura/angular/alurapic/src/app/photos/photos.module.ts
+++ Alura/angular/alurapic/src/app/photos/photos.module.ts
@@ -5,9 +5,19 @@ import { HttpClientModule } from "@angular/common/http";
 import { PhotoComponent } from "./photo/photo.component";
 import { PhotoListComponent } from "./photo-list/photo-list.component";
 import { PhotoFormComponent } from "./photo-form/photo-form.component";
+import { PhotosComponent } from "./photo-list/photos/photos.component";
+import { FilterByDescription } from "./photo-list/filter-by-description.pipe";
+import { LoadButtonComponent } from './photo-list/load-button/load-button.component';
 
 @NgModule({
-  declarations: [PhotoComponent, PhotoListComponent, PhotoFormComponent],
+  declarations: [
+    PhotoComponent,
+    PhotoListComponent,
+    PhotoFormComponent,
+    PhotosComponent,
+    FilterByDescription,
+    LoadButtonComponent
+  ],
   imports: [HttpClientModule, CommonModule],
   providers: [],
   exports: []
