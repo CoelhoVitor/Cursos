afc4bd9953bff27d40a98126da2696194d8e32e8
Vitor Coelho da Silva <vitorvcs@hotmail.com>
2020-02-15 17:47:43 -0300

    module2 xamarin2 course

--- Alura/xamarin/TestDrive/TestDrive/TestDrive/Views/DetalheView.xaml.cs
+++ Alura/xamarin/TestDrive/TestDrive/TestDrive/Views/DetalheView.xaml.cs
@@ -4,100 +4,35 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using TestDrive.Models;
+using TestDrive.ViewModels;
 using Xamarin.Forms;
 
 namespace TestDrive.Views
 {
     public partial class DetalheView : ContentPage
-    {         
+    {
         public Veiculo Veiculo { get; set; }
-
-        public string TextoFreioABS
-        {
-            get
-            {
-                return string.Format("Freio ABS - R$ {0}", Veiculo.FREIO_ABS);
-            }
-        }
-
-        public string TextoArCondicionado
-        {
-            get
-            {
-                return string.Format("Ar-condicionado - R$ {0}", Veiculo.AR_CONDICIONADO);
-            }
-        }
-
-        public string TextoMP3Player
-        {
-            get
-            {
-                return string.Format("MP3 Player - R$ {0}", Veiculo.MP3_PLAYER);
-            }
-        }
-
-        public bool TemFreioABS
-        {
-            get
-            {
-                return Veiculo.TemFreioABS;
-            }
-            set
-            {
-                Veiculo.TemFreioABS = value;
-
-                OnPropertyChanged();
-                OnPropertyChanged(nameof(ValorTotal));
-            }
-        }
-
-        public bool TemArCondicionado
-        {
-            get
-            {
-                return Veiculo.TemArCondicionado;
-            }
-            set
-            {
-                Veiculo.TemArCondicionado = value;
-                OnPropertyChanged();
-                OnPropertyChanged(nameof(ValorTotal));
-            }
-        }
-
-        public bool TemMP3Player
-        {
-            get
-            {
-                return Veiculo.TemMP3Player;
-            }
-            set
-            {
-                Veiculo.TemMP3Player = value;
-                OnPropertyChanged();
-                OnPropertyChanged(nameof(ValorTotal));
-            }
-        }
-
-        public string ValorTotal
-        {
-            get
-            {
-                return Veiculo.PrecoTotalFormatado;                
-            }
-        }
 
         public DetalheView(Veiculo veiculo)
         {
             InitializeComponent();
-
-            Veiculo = veiculo;
-            BindingContext = this;
+            this.Veiculo = veiculo;
+            this.BindingContext = new DetalheViewModel(veiculo);
         }
 
-        private void buttonProximo_Clicked(object sender, EventArgs e)
+        protected override void OnAppearing()
         {
-            Navigation.PushAsync(new AgendamentoView(Veiculo));
+            base.OnAppearing();
+            MessagingCenter.Subscribe<Veiculo>(this, "Proximo", (msg) =>
+            {
+                Navigation.PushAsync(new AgendamentoView(msg));
+            });
+        }
+
+        protected override void OnDisappearing()
+        {
+            base.OnDisappearing();
+            MessagingCenter.Unsubscribe<Veiculo>(this, "Proximo");
         }
     }
 }
