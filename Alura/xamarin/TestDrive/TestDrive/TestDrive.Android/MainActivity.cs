f037fdeb4cf3fcbd2f09c1b5e77d62a9a8a0b6ce
Vitor Coelho da Silva <vitorvcs@hotmail.com>
2020-03-22 13:23:17 -0300

    module1 xamarin4 course

--- Alura/xamarin/TestDrive/TestDrive/TestDrive.Android/MainActivity.cs
+++ Alura/xamarin/TestDrive/TestDrive/TestDrive.Android/MainActivity.cs
@@ -6,12 +6,21 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using TestDrive.Media;
+using TestDrive.Droid;
+using Xamarin.Forms;
+using Android.Content;
+using Android.Provider;
+using Java.IO;
 
+[assembly: Xamarin.Forms.Dependency(typeof(MainActivity))]
 namespace TestDrive.Droid
 {
     [Activity(Label = "TestDrive", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
-    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity, ICamera
     {
+        private static Java.IO.File arquivoImagem;
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             TabLayoutResource = Resource.Layout.Tabbar;
@@ -28,6 +37,49 @@ namespace TestDrive.Droid
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
 
             base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+
+        public void TirarFoto()
+        {
+            Intent intent = new Intent(MediaStore.ActionImageCapture);
+
+            arquivoImagem = PegarArquivoImagem();
+
+            intent.PutExtra(MediaStore.ExtraOutput, Android.Net.Uri.FromFile(arquivoImagem));
+
+            var activity = Forms.Context as Activity;
+            activity.StartActivityForResult(intent, 0);
+        }
+
+        private static File PegarArquivoImagem()
+        {
+            Java.IO.File arquivoImagem;
+            Java.IO.File diretorio = new Java.IO.File(Android.OS.Environment.GetExternalStoragePublicDirectory(
+                Android.OS.Environment.DirectoryPictures), "Imagens");
+
+            if (!diretorio.Exists())
+                diretorio.Mkdirs();
+
+            arquivoImagem = new Java.IO.File(diretorio, "MinhaFoto.jpg");
+            return arquivoImagem;
+        }
+
+        protected override void OnActivityResult(int requestCode, Result resultCode, Intent data)
+        {
+            base.OnActivityResult(requestCode, resultCode, data);
+
+            if (resultCode == Result.Ok)
+            {
+                byte[] bytes;
+
+                using (var stream = new Java.IO.FileInputStream(arquivoImagem))
+                {
+                    bytes = new byte[arquivoImagem.Length()];
+                    stream.Read(bytes);
+                }
+
+                MessagingCenter.Send<byte[]>(bytes, "FotoTirada");
+            }
         }
     }
 }
